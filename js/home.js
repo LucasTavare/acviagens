35c5f999ce19ee0d8b1183fc4920f1f0aaee8c24
Lucas Protestato Tavares <101677196+LucasTavare@users.noreply.github.com>
2023-08-09 08:59:55 -0300

    front pagina inicial feita

---
+++ js/home.js
@@ -0,0 +1,86 @@
+const abaHome = () => {
+
+
+
+        $('#abaHome').addClass('tab-ativo')
+        
+        $('#abaAviao').removeClass('tab-ativo')
+
+        $('#abaInter').removeClass('tab-ativo')
+
+        $('#abaSup').removeClass('tab-ativo')
+
+        $('#abaCruzeiro').removeClass('tab-ativo')
+
+        
+      }
+
+      const abaAviao = () => {
+
+
+        $('#abaAviao').addClass('tab-ativo')
+
+        $('#abaHome').removeClass('tab-ativo')
+
+        $('#abaCruzeiro').removeClass('tab-ativo')
+
+        $('#abaSup').removeClass('tab-ativo')
+
+        $('#abaInter').removeClass('tab-ativo')
+
+        
+      }
+
+      const abaCruzeiro = () => {
+
+
+
+
+        $('#abaCruzeiro').addClass('tab-ativo')
+
+        $('#abaHome').removeClass('tab-ativo')
+
+        $('#abaAviao').removeClass('tab-ativo')
+
+        $('#abaSup').removeClass('tab-ativo')
+
+        $('#abaInter').removeClass('tab-ativo')
+
+        
+      }
+
+      const abaInter = () => {
+
+
+
+
+
+        $('#abaInter').addClass('tab-ativo')
+                
+        $('#abaHome').removeClass('tab-ativo')
+
+        $('#abaAviao').removeClass('tab-ativo')
+
+        $('#abaSup').removeClass('tab-ativo')
+
+        $('#abaCruzeiro').removeClass('tab-ativo')
+
+        
+      }
+
+      const abaSup = () => {
+
+
+
+        $('#abaSup').addClass('tab-ativo')
+
+        $('#abaHome').removeClass('tab-ativo')
+
+        $('#abaAviao').removeClass('tab-ativo')
+
+        $('#abaInter').removeClass('tab-ativo')
+
+        $('#abaCruzeiro').removeClass('tab-ativo')
+
+        
+      }

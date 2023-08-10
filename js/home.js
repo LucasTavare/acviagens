b52d055e572209155bcd63f8e595da5ce8aaf727
Lucas Protestato Tavares <101677196+LucasTavare@users.noreply.github.com>
2023-08-10 00:15:53 -0300

    modal login iniciado

--- js/home.js
+++ js/home.js
@@ -1,86 +1,86 @@
-const abaHome = () => {
+// const abaHome = () => {
 
 
 
-        $('#abaHome').addClass('tab-ativo')
+//         $('#abaHome').addClass('tab-ativo')
         
-        $('#abaAviao').removeClass('tab-ativo')
+//         $('#abaAviao').removeClass('tab-ativo')
 
-        $('#abaInter').removeClass('tab-ativo')
+//         $('#abaInter').removeClass('tab-ativo')
 
-        $('#abaSup').removeClass('tab-ativo')
+//         $('#abaSup').removeClass('tab-ativo')
 
-        $('#abaCruzeiro').removeClass('tab-ativo')
-
-        
-      }
-
-      const abaAviao = () => {
-
-
-        $('#abaAviao').addClass('tab-ativo')
-
-        $('#abaHome').removeClass('tab-ativo')
-
-        $('#abaCruzeiro').removeClass('tab-ativo')
-
-        $('#abaSup').removeClass('tab-ativo')
-
-        $('#abaInter').removeClass('tab-ativo')
+//         $('#abaCruzeiro').removeClass('tab-ativo')
 
         
-      }
+//       }
 
-      const abaCruzeiro = () => {
-
-
+//       const abaAviao = () => {
 
 
-        $('#abaCruzeiro').addClass('tab-ativo')
+//         $('#abaAviao').addClass('tab-ativo')
 
-        $('#abaHome').removeClass('tab-ativo')
+//         $('#abaHome').removeClass('tab-ativo')
 
-        $('#abaAviao').removeClass('tab-ativo')
+//         $('#abaCruzeiro').removeClass('tab-ativo')
 
-        $('#abaSup').removeClass('tab-ativo')
+//         $('#abaSup').removeClass('tab-ativo')
 
-        $('#abaInter').removeClass('tab-ativo')
+//         $('#abaInter').removeClass('tab-ativo')
 
         
-      }
+//       }
 
-      const abaInter = () => {
-
-
+//       const abaCruzeiro = () => {
 
 
 
-        $('#abaInter').addClass('tab-ativo')
+
+//         $('#abaCruzeiro').addClass('tab-ativo')
+
+//         $('#abaHome').removeClass('tab-ativo')
+
+//         $('#abaAviao').removeClass('tab-ativo')
+
+//         $('#abaSup').removeClass('tab-ativo')
+
+//         $('#abaInter').removeClass('tab-ativo')
+
+        
+//       }
+
+//       const abaInter = () => {
+
+
+
+
+
+//         $('#abaInter').addClass('tab-ativo')
                 
-        $('#abaHome').removeClass('tab-ativo')
+//         $('#abaHome').removeClass('tab-ativo')
 
-        $('#abaAviao').removeClass('tab-ativo')
+//         $('#abaAviao').removeClass('tab-ativo')
 
-        $('#abaSup').removeClass('tab-ativo')
+//         $('#abaSup').removeClass('tab-ativo')
 
-        $('#abaCruzeiro').removeClass('tab-ativo')
-
-        
-      }
-
-      const abaSup = () => {
-
-
-
-        $('#abaSup').addClass('tab-ativo')
-
-        $('#abaHome').removeClass('tab-ativo')
-
-        $('#abaAviao').removeClass('tab-ativo')
-
-        $('#abaInter').removeClass('tab-ativo')
-
-        $('#abaCruzeiro').removeClass('tab-ativo')
+//         $('#abaCruzeiro').removeClass('tab-ativo')
 
         
-      }
+//       }
+
+//       const abaSup = () => {
+
+
+
+//         $('#abaSup').addClass('tab-ativo')
+
+//         $('#abaHome').removeClass('tab-ativo')
+
+//         $('#abaAviao').removeClass('tab-ativo')
+
+//         $('#abaInter').removeClass('tab-ativo')
+
+//         $('#abaCruzeiro').removeClass('tab-ativo')
+
+        
+//       }
